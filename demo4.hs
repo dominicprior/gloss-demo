cefd7662ccdd12c0d84c81e539b7832124e9771d
Dominic Prior <dominicprior@yahoo.co.uk>
2016-04-13 20:48:40 +0100

    Add 4-5.txt and tweak demo4 and demo5

--- demo4.hs
+++ demo4.hs
@@ -6,6 +6,6 @@ draw (x,y) = Pictures (map f [1..20])
   where f n = Rotate (x*n*0.1) (blob y)
 blob r = Translate r 0 (Color c (ThickCircle 10 20))
 c = withAlpha 0.3 azure
-ev (EventMotion (x,y)) _ = (x,y)
+ev (EventMotion xy) _ = xy
 ev _ w = w
 step _ w = w
--- demo5.hs
+++ demo5.hs
@@ -1,11 +1,12 @@
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 win = InWindow "" (400, 300) (10, 10)
-main = play win white 20 (replicate 30 (0,0)) draw ev step
-draw (_:blobs) = Pictures (map drawBlob blobs)
-drawBlob (x,y) = Translate x y (Color c (ThickCircle 5 10))
+main = play win white 60 (replicate 30 (0,0)) draw ev step
+draw (_:blobs) = Pictures (map blob blobs)
+blob (x,y) = Translate x y (Color c (ThickCircle 5 10))
 c = withAlpha 0.3 azure
 ev (EventMotion xy) (_:b) = xy:b
 ev _ w = w
 step _ (m:blobs) = m : (zipWith drift blobs (m:blobs))
-drift (x,y) (u,v) = (0.9 * x + 0.1 * u, 0.9 * y + 0.1 * v)
+drift (x,y) (u,v) = (0.9 * x + 0.1 * u,
+                     0.9 * y + 0.1 * v)
